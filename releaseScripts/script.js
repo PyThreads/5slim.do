3d97e549bec580b05cc29ce8d21d48ea3135baaf
joseereyes <mailtojosereyes@gmail.com>
2025-06-07 16:00:30 -0400

    ll

--- releaseScripts/script.js
+++ releaseScripts/script.js
@@ -2,10 +2,17 @@
 var adminList = [
    {
         _id: 1,
-        name: "Jose",
+        firstName: "Jose",
         lastName: "Reyes",
         fullName: "Jose Reyes",
         email: "josereyes@pythreads.com"
+    },
+    {
+        _id: 2,
+        firstName: "Lucia",
+        lastName: "Figuereo",
+        fullName: "Jose Reyes",
+        email: "luciagfiguereo@gmail.com"
     }
 ]
 
